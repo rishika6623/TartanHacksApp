2c8c11c4caf6b1f98b323510ea26908cbf9e7cf5
rishika6623 <rishika6623@gmail.com>
2024-02-03 12:10:45 -0500

    Switch to StoryBoard

---
+++ StoryBoard/Tartan Hacks StoryBoard Builder/Tartan Hacks StoryBoard Builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tartan Hacks StoryBoard Builder
+//
+//  Created by Rishika Agarwal on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
